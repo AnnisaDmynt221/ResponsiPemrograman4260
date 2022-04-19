5277a932a6698839531039065c6e433b99357328
AnnisaDmynt221 <97298883+AnnisaDmynt221@users.noreply.github.com>
2022-04-19 16:28:53 +0700

    ResponsiPemrograman4260

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4260
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NAMA    : ANNISA DAMAYANTI");
+            Console.WriteLine("NIM     : 21.11.4260");
+            Console.WriteLine("KELAS   : 21 IF 07");
+
+            Console.WriteLine("\n");
+
+            karyawan karyawan1 = new karyawan(199872111, "MUTYA", 3000000);
+            karyawan karyawan2 = new karyawan(196704260, "RAYYAN", 4000000);
+            karyawan karyawan3 = new karyawan(190674567, "JULI", -2000000);
+
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.NAMA, karyawan1.GajiBulanan);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.NAMA, karyawan2.GajiBulanan);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.NAMA, karyawan3.GajiBulanan);
+
+            Console.WriteLine("\n\nAsiiiiikkkkk dapat tambahan gaji 10% nih !!!\n");
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.NAMA, karyawan1.TambahGaji);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.NAMA, karyawan2.TambahGaji);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.NAMA, karyawan3.GajiBulanan);
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ karyawan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4260
+{
+    class karyawan
+    {
+        public int NIK { get; set; }
+        public string NAMA { get; set; }
+        public int GajiBulanan { get; set; }
+        public int TambahGaji { get; set; }
+
+        public karyawan(int nik, string nama, int gajibulanan)
+        {
+            this.NIK = nik;
+            this.NAMA = nama;
+
+            if (gajibulanan < 0)
+            {
+                this.GajiBulanan = 0;
+                TambahGaji = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+            else
+            {
+                this.GajiBulanan = gajibulanan;
+                TambahGaji = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+        }
+    }
+}
